3677ce1f80ef8986b83e730253cd4ff11434686b
Liviana <liviana.deisvaldi@gmail.com>
2025-09-21 13:14:22 +0200

    ex00: subject tests added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -5,10 +5,25 @@ int main()
 	std::cout << min(3, 4) << std::endl;
 	std::cout << min("ciao", "c") << std::endl;
 	
-	std::string a = "ciao";
-	std::string b = "hello";
-	swap(a, b);
-	std::cout << a << std::endl;
+	std::string e = "ciao";
+	std::string f = "hello";
+	swap(e, f);
+	std::cout << e << std::endl;
 
 	std::cout << max(21.05, 23.00) << std::endl;
+
+	std::cout << "SUBJECT TESTS\n";
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
