c8c1480e2f69d5a90335c21805ba9d7f6f5cab45
Liviana <liviana.deisvaldi@gmail.com>
2025-09-23 00:28:06 +0200

    FIX: const and non const in the same template as parameters

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -2,16 +2,8 @@
 
 #include <iostream>
 
-template <typename T>
-void	iter(T* adress, int size, void (f)(T&))
-{
-	for (int i = 0; i < size; i++)
-		f(adress[i]);
-	return ;
-}
-
-template <typename T>
-void	iter(T* adress, int size, void (f)(const T&))
+template <typename T, typename F>
+void	iter(T* adress, int size, void (f)(F&))
 {
 	for (int i = 0; i < size; i++)
 		f(adress[i]);
